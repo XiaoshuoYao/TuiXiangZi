87652b6ded79538c48f8dcd7d9a35c222838066e
Xiaoshuo Yao <x5yao@ucsd.edu>
2026-03-16 13:15:08 -0700

    Init

---
+++ Source/TuiXiangZi/TuiXiangZi.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TuiXiangZi.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TuiXiangZi, "TuiXiangZi" );
+
+DEFINE_LOG_CATEGORY(LogTuiXiangZi)
+ 
---
+++ Source/TuiXiangZi/TuiXiangZiGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TuiXiangZiGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATuiXiangZiGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATuiXiangZiGameMode();
+};
+
+
+
